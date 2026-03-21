630ab072ced8e15baf7afd8c7ccf6f341839ae7e
VanhDepTraiBoDoiQua <mgmgd2004@gmail.com>
2026-03-21 08:20:30 +0700

    26/03/21 - Invoices 1. Add Get detailed invoice by id API 2. Add exception for invoice logic 3. Ngan Ngan Ngan
    Signed-off-by: VanhDepTraiBoDoiQua <mgmgd2004@gmail.com>

--- src/main/java/com/backend/billiards_management/repositories/InvoiceRepository.java
+++ src/main/java/com/backend/billiards_management/repositories/InvoiceRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Date;
+import java.util.Optional;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
     Page<Invoice> findByCreatedAtBetweenAndDeletedFalse(Date startDate, Date endDate, Pageable pageable);
+
+    Optional<Invoice> findByIdAndDeletedFalse(Integer invoiceId);
 }
--- src/main/java/com/backend/billiards_management/services/invoice/InvoiceService.java
+++ src/main/java/com/backend/billiards_management/services/invoice/InvoiceService.java
@@ -1,9 +1,12 @@
 package com.backend.billiards_management.services.invoice;
 
+import com.backend.billiards_management.dtos.request.invoice.InvoiceDetailRes;
 import com.backend.billiards_management.dtos.request.invoice.InvoiceReq;
 import com.backend.billiards_management.dtos.response.invoice.InvoiceRes;
 import org.springframework.data.domain.Page;
 
 public interface InvoiceService {
     Page<InvoiceRes> getInvoicesByRange(InvoiceReq invoiceReq);
+
+    InvoiceDetailRes getInvoiceById(Integer invoiceId);
 }
